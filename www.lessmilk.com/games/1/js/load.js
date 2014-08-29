b2eb71a75a9d88025227dd094f6dfde90c5e3d5f
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2014-08-29 17:58:34 -0300

    Exemplo funcionando localmente

--- www.lessmilk.com/games/1/js/load.js
+++ www.lessmilk.com/games/1/js/load.js
@@ -13,11 +13,11 @@ Game.Load.prototype = {
 	    game.stage.backgroundColor = '#34495e';
 	    label = game.add.text(w/2, h/2, 'loading...', { font: '30px Arial', fill: '#fff' });
 		label.anchor.setTo(0.5, 0.5);
-
-		game.load.spritesheet('player', 'images/player.png', 20, 24);
-		game.load.spritesheet('enemy1', 'images/enemy1.png', 32, 36);
-		game.load.spritesheet('enemy2', 'images/enemy2.png', 28, 40);
-		game.load.audio('hit', 'sounds/hit.wav');
+		var thisdir = "file:///home/ceu/ceu-sdl/lessmilk/www.lessmilk.com/games/1";
+		game.load.spritesheet('player', thisdir+'/images/player.png', 20, 24);
+		game.load.spritesheet('enemy1', thisdir+'/images/enemy1.png', 32, 36);
+		game.load.spritesheet('enemy2', thisdir+'/images/enemy2.png', 28, 40);
+		game.load.audio('hit', thisdir+'/sounds/hit.wav');
 
 	},
 	create: function () {
