7909ecf1c8033d908a8865567031375bd8766495
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2014-09-20 15:04:09 -0300

    run 2 local

--- www.lessmilk.com/games/2/js/load.js
+++ www.lessmilk.com/games/2/js/load.js
@@ -16,18 +16,19 @@ Game.Load.prototype = {
 	    label = game.add.text(w/2, h/2, 'loading...', { font: '30px Arial', fill: '#fff' });
 		label.anchor.setTo(0.5, 0.5);
 
-		game.load.image('bg', 'images/bg.png');
-		game.load.image('player', 'images/player.png');
-		game.load.image('fire', 'images/fire.png');
-	    game.load.image('bonus', 'images/bonus.png');
-		game.load.image('pixel', 'images/pixel.png');
-		game.load.spritesheet('bullet', 'images/bullet.png', 16, 32);
-		game.load.image('enemy', 'images/enemy.png');
-		game.load.audio('hit', 'sounds/hit.wav');
-		game.load.audio('fire', 'sounds/fire.wav');
-		game.load.audio('exp', 'sounds/exp.wav');
-		game.load.audio('dead', 'sounds/dead.wav');
-	    game.load.audio('bonus', 'sounds/bonus.wav');
+		var thisdir = "file:///home/ceu/ceu-sdl/lessmilk/www.lessmilk.com/games/2";
+		game.load.image('bg', thisdir+'/images/bg.png');
+		game.load.image('player', thisdir+'/images/player.png');
+		game.load.image('fire', thisdir+'/images/fire.png');
+	    game.load.image('bonus', thisdir+'/images/bonus.png');
+		game.load.image('pixel', thisdir+'/images/pixel.png');
+		game.load.spritesheet('bullet', thisdir+'/images/bullet.png', 16, 32);
+		game.load.image('enemy', thisdir+'/images/enemy.png');
+		game.load.audio('hit', thisdir+'/sounds/hit.wav');
+		game.load.audio('fire', thisdir+'/sounds/fire.wav');
+		game.load.audio('exp', thisdir+'/sounds/exp.wav');
+		game.load.audio('dead', thisdir+'/sounds/dead.wav');
+	    game.load.audio('bonus', thisdir+'/sounds/bonus.wav');
 	},
 	create: function () {
 		game.state.start('Intro');
