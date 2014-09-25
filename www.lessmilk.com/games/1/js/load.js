5ba9116cf3e6277b6d715bd215f8c94fcee36c44
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2014-09-25 10:07:58 -0300

    fixed path, now relative

--- www.lessmilk.com/games/1/js/load.js
+++ www.lessmilk.com/games/1/js/load.js
@@ -13,7 +13,9 @@ Game.Load.prototype = {
 	    game.stage.backgroundColor = '#34495e';
 	    label = game.add.text(w/2, h/2, 'loading...', { font: '30px Arial', fill: '#fff' });
 		label.anchor.setTo(0.5, 0.5);
-		var thisdir = "file:///home/ceu/ceu-sdl/lessmilk/www.lessmilk.com/games/1";
+	    var path = document.location.pathname;
+        var dir = path.substring(0, path.lastIndexOf('/'));
+		var thisdir = "file://" + dir;
 		game.load.spritesheet('player', thisdir+'/images/player.png', 20, 24);
 		game.load.spritesheet('enemy1', thisdir+'/images/enemy1.png', 32, 36);
 		game.load.spritesheet('enemy2', thisdir+'/images/enemy2.png', 28, 40);
